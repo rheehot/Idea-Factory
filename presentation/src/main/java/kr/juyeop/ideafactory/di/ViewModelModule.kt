09de68765a2458814b7748a033428c1665841df2
김주엽 <49600974+Kim-Ju-Yeop@users.noreply.github.com>
2020-06-20 09:57:37 +0900

    [ADD] TabLayout
    MainActivity - TabLayout 및 각 Fragment를 구현하였습니다.

--- presentation/src/main/java/kr/juyeop/ideafactory/di/ViewModelModule.kt
+++ presentation/src/main/java/kr/juyeop/ideafactory/di/ViewModelModule.kt
@@ -4,6 +4,9 @@ import kr.juyeop.ideafactory.viewmodel.fragment.InventoryViewModel
 import kr.juyeop.ideafactory.viewmodel.activity.MainViewModel
 import kr.juyeop.ideafactory.viewmodel.activity.NameViewModel
 import kr.juyeop.ideafactory.viewmodel.activity.SplashViewModel
+import kr.juyeop.ideafactory.viewmodel.fragment.MeetingViewModel
+import kr.juyeop.ideafactory.viewmodel.fragment.ProductViewModel
+import kr.juyeop.ideafactory.viewmodel.fragment.TradingViewModel
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
@@ -11,5 +14,9 @@ val viewModelModule = module {
     viewModel { SplashViewModel() }
     viewModel { NameViewModel() }
     viewModel { MainViewModel() }
+
     viewModel { InventoryViewModel() }
+    viewModel { ProductViewModel() }
+    viewModel { TradingViewModel() }
+    viewModel { MeetingViewModel() }
 }
--- presentation/src/main/java/kr/juyeop/ideafactory/view/activity/MainActivity.kt
+++ presentation/src/main/java/kr/juyeop/ideafactory/view/activity/MainActivity.kt
@@ -1,5 +1,8 @@
 package kr.juyeop.ideafactory.view.activity
 
+import com.google.android.material.tabs.TabLayout
+import kotlinx.android.synthetic.main.activity_main.*
+import kr.juyeop.ideafactory.R
 import kr.juyeop.ideafactory.base.BaseActivity
 import kr.juyeop.ideafactory.databinding.ActivityMainBinding
 import kr.juyeop.ideafactory.view.fragment.InventoryFragment
@@ -24,8 +27,24 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
 
     @Override
     override fun init() {
-//        val fragmentTransaction = supportFragmentManager.beginTransaction()
-//        fragmentTransaction.replace(R.id.frameLayout, homeFragment).commitAllowingStateLoss()
-//        selectedEvent()
+        val fragmentTransaction = supportFragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frameLayout, inventoryFragment).commitAllowingStateLoss()
+        selectedEvent()
+    }
+
+    fun selectedEvent(){
+        tabs.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener{
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                val fragmentTransaction = supportFragmentManager.beginTransaction()
+                when(tab.position){
+                    0-> fragmentTransaction.replace(R.id.frameLayout, inventoryFragment).commitAllowingStateLoss()
+                    1-> fragmentTransaction.replace(R.id.frameLayout, productFragment).commitAllowingStateLoss()
+                    2-> fragmentTransaction.replace(R.id.frameLayout, tradingFragment).commitAllowingStateLoss()
+                    3-> fragmentTransaction.replace(R.id.frameLayout, meetingFragment).commitAllowingStateLoss()
+                }
+            }
+            override fun onTabReselected(tab: TabLayout.Tab) {}
+            override fun onTabUnselected(tab: TabLayout.Tab) {}
+        })
     }
 }
